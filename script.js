f023d402e298ab573bbd55352f3893b207e3c62f
raajayy <103039593+raajayy@users.noreply.github.com>
2023-12-24 19:12:52 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function moveButton()
+    {
+        var x=Math.random() * ( window.innerWidth - document.getElementById('noButton').offsetWidth);
+        var y= Math.random() * ( window.innerHeight - document.getElementById('noButton').offsetHeight);
+        document.getElementById('noButton').style.left=`${x}px`;
+        document.getElementById('noButton').style.top=`${y}px`;
+    }
